c86258f1ad450123ba47838b17efc175ad7b3874
vaishnav ingole <vaishnavingole2507@gmail.com>
2025-09-13 19:46:32 +0530

    user registration 2

--- src/main/java/com/app/ecommerce/entity/BaseEntity.java
+++ src/main/java/com/app/ecommerce/entity/BaseEntity.java
@@ -5,6 +5,9 @@ import java.util.Date;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseEntity {
 
 	@CreationTimestamp
